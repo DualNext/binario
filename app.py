c58440e893f4216a0b38caca02c00592425f8988
DualNext <thiagofranssa@gmail.com>
2025-03-18 14:58:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ sidebar = st.sidebar
 logo = 'logo.png'  # Substitua pelo caminho correto para o seu logo
 sidebar.image(logo, use_container_width=True)
 
+# Adicionar instruções para o usuário
+sidebar.markdown("Por favor, carregue um arquivo CSV contendo os dados FTIR para análise.")
+
 # Widget de upload de arquivo na barra lateral
 uploaded_file = sidebar.file_uploader('Use um arquivo CSV (separado por vírgula)', type="csv")
 
@@ -49,38 +52,38 @@ if uploaded_file is not None:
     # Criar colunas para o gráfico e resultados
     col1 = st.columns(1)[0]  # Pegando a primeira (e única) coluna
 
-    # Exibir um gráfico de linhas com os dados filtrados na primeira coluna
-    with col1:
-        fig = plt.figure(figsize=(13, 6))
-        plt.style.use("cyberpunk")
-
-        # Criar sua linha
-        plt.plot(dados_coletados, lw=2, color='green')  # Linha na cor verde
-
-        # Adicionar efeitos de brilho
-        mplcyberpunk.add_glow_effects()
-
-        # Formatação do gráfico
-        plt.gca().invert_xaxis()
-        plt.title('Espectro FTIR', pad=10, fontsize=30, fontname='Cambria')
-        plt.xlabel('Número de Onda ($\mathregular{cm^-¹}$)', labelpad=17, fontsize=26, fontname='Cambria')
-        plt.ylabel('Transmitância Normalizada', labelpad=15, fontsize=28, fontname='Cambria')
-        plt.xticks(np.arange(900, 1800 + 100, 100), fontsize=18, fontname='Cambria')
-        plt.gca().tick_params(axis='x', pad=20)  # Ajusta o espaço entre os rótulos e a linha
-        plt.xlim(1800, 900)
-        plt.ylim(dados_coletados.min().min() - 0.5, 100.5)
-        plt.yticks(fontsize=18, fontname='Cambria')
-        plt.gca().tick_params(axis='y', pad=20)  # Ajusta o espaço entre os rótulos e a linha
-        st.pyplot(fig)
-
-    # Exibir a mensagem e o botão "Continuar" apenas se for permitido
+    # Exibir gráfico FTIR antes de pressionar o botão
     if st.session_state.show_button:
+        with col1:
+            fig = plt.figure(figsize=(13, 6))
+            plt.style.use("cyberpunk")
+
+            # Criar sua linha
+            plt.plot(dados_coletados, lw=2, color='green')  # Linha na cor verde
+
+            # Adicionar efeitos de brilho
+            mplcyberpunk.add_glow_effects()
+
+            # Formatação do gráfico
+            plt.gca().invert_xaxis()
+            plt.title('Espectro FTIR', pad=10, fontsize=30, fontname='Cambria')
+            plt.xlabel('Número de Onda ($\mathregular{cm^-¹}$)', labelpad=17, fontsize=26, fontname='Cambria')
+            plt.ylabel('Transmitância Normalizada', labelpad=15, fontsize=28, fontname='Cambria')
+            plt.xticks(np.arange(900, 1800 + 100, 100), fontsize=18, fontname='Cambria')
+            plt.gca().tick_params(axis='x', pad=20)  # Ajusta o espaço entre os rótulos e a linha
+            plt.xlim(1800, 900)
+            plt.ylim(dados_coletados.min().min() - 0.5, 100.5)
+            plt.yticks(fontsize=18, fontname='Cambria')
+            plt.gca().tick_params(axis='y', pad=20)  # Ajusta o espaço entre os rótulos e a linha
+            st.pyplot(fig)
+
+        # Exibir a mensagem e o botão "Continuar" apenas se for permitido
         st.info('Espectro medido corretamente! Clique em "continuar"')
 
         if st.button('Continuar'):
             st.session_state.show_button = False  # Ocultar mensagem e botão após o clique
 
-    # Exibir o gráfico de barras ou pizza apenas após o botão ser pressionado
+    # Exibir o gráfico de pizza com as probabilidades após o botão ser pressionado
     if not st.session_state.show_button:
         
         # Carregar os modelos treinados
@@ -118,10 +121,14 @@ if uploaded_file is not None:
             cores = ['gray', 'green']  # Cinza para Brucelose, Verde para Controle
     
         # Exibir gráfico de pizza com as probabilidades
-        fig, ax = plt.subplots(figsize=(4, 4))
+        fig, ax = plt.subplots(figsize=(6, 6))
         ax.pie([probabilidade_bru, probabilidade_controle], labels=classes, autopct='%1.2f%%', startangle=90, colors=cores)
-        ax.set_title('Probabilidades de Diagnóstico', fontsize=14)
+        ax.set_title('Distribuição das Probabilidades', fontsize=14)
         st.pyplot(fig)
+
+        # Exibir a classe predita junto com as probabilidades
+        classe_predita = classes[np.argmax(prob)]  # Classe com maior probabilidade
+        st.write(f"Diagnóstico: {classe_predita}")
 
 else:
     st.markdown('''<h1 style="color: orange; font-size: 35px;">Diagnóstico de Brucelose Bovina</h1>''', unsafe_allow_html=True)
